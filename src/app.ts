cab83e8cc76b19144c073372580abc2fd450f7d9
Gamaranto <gamarantor@gmail.com>
2020-05-11 15:33:35 +0200

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { getStatus } from "./get-status";
 
 const app = express();
 const port = process.env.PORT || 8081;
 
+app.use(cors());
 app.get("/", async (req, res) => {
   let status = await getStatus();
   res.setHeader("Content-Type", "application/json");
