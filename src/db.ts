e0702bbc2fc77aaac21b2e7f6bea062c28c79e55
Gamaranto <gamarantor@gmail.com>
2020-05-19 22:07:23 +0200

    Add BURNER_ADDRESS and exchanges How to

--- src/db.ts
+++ src/db.ts
@@ -2,47 +2,47 @@ import low from "lowdb";
 import FileAsync from "lowdb/adapters/FileAsync";
 
 type Exchange = {
-    sender: string,
-    recipient: string,
-    senderMemo: string,
-    xmrAddress: string,
-    amount: number,
-    fees: number,
-    date: Date,
-    blockHeight: number,
-    price: number,
-    amountUSD: number
+  sender: string;
+  recipient: string;
+  senderMemo: string;
+  xmrAddress: string;
+  amount: number;
+  fees: number;
+  date: Date;
+  blockHeight: number;
+  price: number;
+  amountUSD: number;
 };
 
 type BlockProcessingError = {
-    blockNumber: number;
-    reason?: string;
-}
-
-type Burn = {
-    amount: number,
-    tokensRecievedAtBlock: number,
-    finalStatus: string,
-    finalizedBlockHash?: string,
-}
-
-type BlockProcessingWarning = {
-    blockNumber: number,
-    message: string
-}
-
-type Schema = {
-    exchanges: Exchange[];
-    sizeDollarPool: number,
-    lastBlockProcessed: number,
-    replenishAmount: number,
-    tokensBurned: number,
-    errors: BlockProcessingError[],
-    warnings: BlockProcessingWarning[],
-    burns: Burn[]
+  blockNumber: number;
+  reason?: string;
 };
 
-const adapter = new FileAsync<Schema>("exchanges.test.json");
+type Burn = {
+  amount: number;
+  tokensRecievedAtBlock: number;
+  finalStatus: string;
+  finalizedBlockHash?: string;
+};
+
+type BlockProcessingWarning = {
+  blockNumber: number;
+  message: string;
+};
+
+type Schema = {
+  exchanges: Exchange[];
+  sizeDollarPool: number;
+  lastBlockProcessed: number;
+  replenishAmount: number;
+  tokensBurned: number;
+  errors: BlockProcessingError[];
+  warnings: BlockProcessingWarning[];
+  burns: Burn[];
+};
+
+const adapter = new FileAsync<Schema>("exchanges.json");
 const db = low(adapter);
 
 export { db, Exchange, BlockProcessingError, BlockProcessingWarning, Burn };
