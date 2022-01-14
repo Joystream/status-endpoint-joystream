236cec15c85499cd800ebe7bb2768755f53a0040
Edvin Dzidic <dzidic@tugraz.student.at>
2022-01-14 11:41:44 +0100

    Fix formatting, tab 4 spaces -> 2 spaces

--- src/app.ts
+++ src/app.ts
@@ -23,35 +23,35 @@ app.get("/", async (req, res) => {
   res.send(status);
 });
 
-app.post("/status", async (req,res) => {
-    const password = req.header("x-api-key");
+app.post("/status", async (req, res) => {
+  const password = req.header("x-api-key");
 
-    if(process.env.PASSWORD !== password) {
-        res.status(401).send({
-            message: "Wrong authentication credentials!"
-        });
-        return;
-    }
-
-    const { exchangeIndex, exchangeStatus } = req.body;
-
-    if(!exchangeIndex || !exchangeStatus) {
-        res.status(400).send({
-            message: "Missing exchangeIndex or exchangeStatus in request body."
-        });
-        return;
-    }
-
-    const error = await setExchangeStatus(exchangeIndex, exchangeStatus);
-
-    if(error) {
-        res.status(400).send({ message: error });
-        return;
-    }
-
-    res.status(200).send({
-        message: `Succesfully updated the status of exchange ${exchangeIndex}`
+  if (process.env.PASSWORD !== password) {
+    res.status(401).send({
+      message: "Wrong authentication credentials!"
     });
+    return;
+  }
+
+  const { exchangeIndex, exchangeStatus } = req.body;
+
+  if (!exchangeIndex || !exchangeStatus) {
+    res.status(400).send({
+      message: "Missing exchangeIndex or exchangeStatus in request body."
+    });
+    return;
+  }
+
+  const error = await setExchangeStatus(exchangeIndex, exchangeStatus);
+
+  if (error) {
+    res.status(400).send({ message: error });
+    return;
+  }
+
+  res.status(200).send({
+    message: `Succesfully updated the status of exchange ${exchangeIndex}`
+  });
 });
 
 app.listen(port, () => {
