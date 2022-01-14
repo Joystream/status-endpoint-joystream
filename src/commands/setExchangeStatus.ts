0415143cdfb7d850488e80bd9115a54c3cfc0425
Edvin Dzidic <dzidic@tugraz.student.at>
2022-01-14 11:40:45 +0100

    Refactor set exchange status code to not duplicate logic, fix invalid status being treated as successful operation by /status

--- src/commands/setExchangeStatus.ts
+++ src/commands/setExchangeStatus.ts
@@ -1,49 +1,13 @@
-import { db, ExchangeStatuses, ExchangeStatus, Schema, Exchange } from '../db';
-import { confirm } from './utils';
+import { setExchangeStatus } from '../setExchangeStatus';
 
 async function main() {
   const index = parseInt(process.argv[2]);
   const status = process.argv[3];
 
-  if (!ExchangeStatuses.includes(status as any)) {
-    console.warn('Invalid status! Available statuses are:', ExchangeStatuses);
-    return;
-  }
+  const error = await setExchangeStatus(index, status, true);
 
-  const { exchanges = [] } = (await db).valueOf() as Schema;
-  const exchange = exchanges[index];
-
-  if (!exchange) {
-    console.warn('No exchange found by given index!');
-    return;
-  }
-
-  if (exchange.status === 'FINALIZED') {
-    console.warn('Cannot change status of FINALIZED exchange!')
-    return;
-  }
-
-  console.log('Exchange that will be updated:', exchange);
-  const confirmed = await confirm(`Are you sure you want to change its status to ${status}?`);
-
-  if (confirmed) {
-    // Update exchange status
-    (await db)
-        .defaults({ exchanges: [] as Exchange[] })
-        .get('exchanges')
-        .get(index)
-        .assign({ status })
-        .value();
-    if ((status as ExchangeStatus) === 'FINALIZED') {
-      // Update totalUSDPaid
-      (await db)
-        .defaults({ totalUSDPaid: 0 })
-        .update('totalUSDPaid', (prev) => prev + exchange.amountUSD)
-        .value();
-    }
-    // Save changes
-    (await db).write()
-    console.log(`Exchange status and totalUSDPaid updated!`);
+  if(error) {
+    console.warn(error);
   }
 }
 
--- src/setExchangeStatus.ts
+++ src/setExchangeStatus.ts
@@ -1,37 +1,47 @@
 import { db, ExchangeStatuses, ExchangeStatus, Schema, Exchange } from './db';
+import { confirm } from './commands/utils';
 
-export async function setExchangeStatus(index: number, status: string) {
+export async function setExchangeStatus(index: number, status: string, calledFromCLI?: boolean) {
   if (!ExchangeStatuses.includes(status as any)) {
-    console.warn("Invalid status! Available statuses are:", ExchangeStatuses);
-    return;
+    return `Invalid status! Available statuses are:, ${ExchangeStatuses}`;
   }
 
   const { exchanges = [] } = (await db).valueOf() as Schema;
   const exchange = exchanges[index];
 
   if (!exchange) {
-    return "No exchange found at given index!";
+    return 'No exchange found by given index!';
   }
 
   if (exchange.status === "FINALIZED") {
-    return "Cannot change status of FINALIZED exchange!";
+    return 'Cannot change status of FINALIZED exchange!';
   }
 
-  // Update exchange status
-  (await db)
-    .defaults({ exchanges: [] as Exchange[] })
-    .get("exchanges")
-    .get(index)
-    .assign({ status })
-    .value();
-  if ((status as ExchangeStatus) === "FINALIZED") {
-    // Update totalUSDPaid
-    (await db)
-      .defaults({ totalUSDPaid: 0 })
-      .update("totalUSDPaid", prev => prev + exchange.amountUSD)
-      .value();
+  let confirmed = false;
+  if(calledFromCLI) {
+    console.log('Exchange that will be updated:', exchange);
+    confirmed = await confirm(`Are you sure you want to change its status to ${status}?`);
   }
-  // Save changes
-  (await db).write();
+
+  if((calledFromCLI && confirmed) || !calledFromCLI) {
+    // Update exchange status
+    (await db)
+      .defaults({ exchanges: [] as Exchange[] })
+      .get("exchanges")
+      .get(index)
+      .assign({ status })
+      .value();
+    if ((status as ExchangeStatus) === "FINALIZED") {
+      // Update totalUSDPaid
+      (await db)
+        .defaults({ totalUSDPaid: 0 })
+        .update("totalUSDPaid", prev => prev + exchange.amountUSD)
+        .value();
+    }
+    // Save changes
+    (await db).write();
+    console.log(`Exchange status and totalUSDPaid updated!`);
+  }
+
   return null;
 }
