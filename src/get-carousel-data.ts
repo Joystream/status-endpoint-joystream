1690ea5ac5d996d959ee7ea901850e8257dce19a
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-04-03 02:03:42 +0200

    Expand carousel-data route with initial proposal fetching and parsing

--- src/get-carousel-data.ts
+++ src/get-carousel-data.ts
@@ -14,14 +14,44 @@ type NFT = {
     thumbnailPhotoId: string;
     thumbnailPhoto: {
       storageBag: {
-        distributionBuckets: Array<{ operators: Array<{ metadata: { nodeEndpoint: string } }> }>;
+        distributionBuckets: Array<{
+          operators: Array<{ metadata: { nodeEndpoint: string } }>;
+        }>;
       };
     };
   };
 };
 
+type Proposal = {
+  title: string;
+  createdAt: string;
+  isFinalized: boolean;
+  status: {
+    __typename: string;
+  };
+  id: string;
+  statusSetAtTime: string;
+  creator: {
+    metadata: {
+      avatar: {
+        avatarUri: string;
+      };
+    };
+  };
+};
+
+const PROPOSAL_STATUS = "ProposalStatus";
+
 const getCarouselData = async () => {
-  const response = await api.qnQuery<{ ownedNfts: Array<NFT> }>(`
+  const result: { nfts: Array<{}>; proposals: Array<{}> } = {
+    nfts: [],
+    proposals: [],
+  };
+
+  const response = await api.qnQuery<{
+    ownedNfts: Array<NFT>;
+    proposals: Array<Proposal>;
+  }>(`
     {
       ownedNfts(limit: 10, orderBy: lastSaleDate_DESC, where: { lastSalePrice_gt: 0 }){
         lastSaleDate,
@@ -45,13 +75,32 @@ const getCarouselData = async () => {
             }
           }
         }
+      },
+      proposals(limit: 10, orderBy: createdAt_DESC) {
+        title
+        createdAt
+        isFinalized
+        status {
+          __typename
+        }
+        id
+        statusSetAtTime
+        creator {
+          metadata {
+            avatar {
+              ... on AvatarUri {
+                avatarUri
+              }
+            }
+          }
+        }
       }
     }
   `);
 
-  if (!response) return [];
+  if (!response) return result;
 
-  return response.ownedNfts.map(
+  result.nfts = response.ownedNfts.map(
     ({
       lastSaleDate,
       lastSalePrice,
@@ -61,18 +110,38 @@ const getCarouselData = async () => {
         title: nftTitle,
         thumbnailPhotoId,
         thumbnailPhoto: {
-          storageBag: { distributionBuckets }
-        }
-      }
+          storageBag: { distributionBuckets },
+        },
+      },
     }) => ({
       nftTitle,
       channelName,
       joyAmount: (Number(lastSalePrice) / 10_000_000_000).toString(),
       lastSaleDate,
       imageUrl: `${distributionBuckets[0].operators[0].metadata.nodeEndpoint}api/v1/assets/${thumbnailPhotoId}`,
-      videoUrl: `https://gleev.xyz/video/${videoId}`
+      videoUrl: `https://gleev.xyz/video/${videoId}`,
     })
   );
+
+  result.proposals = response.proposals.map(
+    ({
+      title,
+      status: { __typename: status },
+      id,
+      creator: {
+        metadata: {
+          avatar: { avatarUri },
+        },
+      },
+    }) => ({
+      title,
+      status: status.substring(PROPOSAL_STATUS.length),
+      link: `https://pioneerapp.xyz/#/proposals/preview/${id}`,
+      img: avatarUri,
+    })
+  );
+
+  return result;
 };
 
 export default getCarouselData;
